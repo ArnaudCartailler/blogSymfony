cec9b830130a61e201d83b0c9288e210329899ac
ArnaudCartailler <arnaud.cartailler@gmail.com>
2019-01-29 16:39:23 +0100

    work on first video done

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -29,10 +34,12 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show()
+    public function show(Article $article)
     {
-        return $this->render('blog/show.html.twig');
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
